f5b5bfa8f2fa86bd46e4dca74f521ebd5ba35601
Shiblee <shibleerahman073@gmail.com>
2026-09-01 19:04:43 +0600

    Adding Config---shiblee

---
+++ config/db_connect.php
@@ -0,0 +1,20 @@
+<?php
+
+$db_host = "localhost";
+$db_user = "root";       
+$db_pass = "";        
+$db_name = "campus_skill_exchange";
+
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
